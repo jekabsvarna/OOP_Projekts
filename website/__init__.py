24bd9503b2882db597362a7c50ed19010fd5c088
jekabsvarna <159711976+jekabsvarna@users.noreply.github.com>
2024-03-05 15:35:17 +0200

    Add files via upload

--- website/__init__.py
+++ website/__init__.py
@@ -4,8 +4,12 @@ from os import path
 from flask_login import LoginManager
 
 db = SQLAlchemy()
-DB_NAME = "database.db"
+DB_NAME = 'database.db'
 
+def create_database(app):
+    if not path.exists('instance/' + DB_NAME):
+        db.create_all(app=app)
+        print('Created Database!')
 
 def create_app():
     app = Flask(__name__)
@@ -19,10 +23,9 @@ def create_app():
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
 
-    from .models import User, Note
-    
-    with app.app_context():
-        db.create_all()
+    from .models import User
+
+    create_database(app)
 
     login_manager = LoginManager()
     login_manager.login_view = 'auth.login'
@@ -32,12 +35,4 @@ def create_app():
     def load_user(id):
         return User.query.get(int(id))
 
-    create_database(app)
-
     return app
-
-
-def create_database(app):
-    if not path.exists('website/' + DB_NAME):
-        db.create_all(app=app)
-        print('Created Database!')
--- website/auth.py
+++ website/auth.py
@@ -1,12 +1,10 @@
 from flask import Blueprint, render_template, request, flash, redirect, url_for
 from .models import User
-from werkzeug.security import generate_password_hash, check_password_hash
-from . import db   ##means from __init__.py import db
+import hashlib
+from. import db
 from flask_login import login_user, login_required, logout_user, current_user
 
-
 auth = Blueprint('auth', __name__)
-
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
@@ -16,7 +14,7 @@ def login():
 
         user = User.query.filter_by(email=email).first()
         if user:
-            if check_password_hash(user.password, password):
+            if user.password == password:
                 flash('Logged in successfully!', category='success')
                 login_user(user, remember=True)
                 return redirect(url_for('views.home'))
@@ -27,40 +25,37 @@ def login():
 
     return render_template("login.html", user=current_user)
 
-
 @auth.route('/logout')
 @login_required
 def logout():
-    logout_user()
-    return redirect(url_for('auth.login'))
-
+        logout_user()
+        return redirect(url_for('auth.login'))
 
 @auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
     if request.method == 'POST':
-        email = request.form.get('email')
-        first_name = request.form.get('firstName')
-        password1 = request.form.get('password1')
-        password2 = request.form.get('password2')
+             email = request.form.get('email')
+             first_name = request.form.get('firstName')
+             password1 = request.form.get('password1')
+             password2 = request.form.get('password2')
 
-        user = User.query.filter_by(email=email).first()
-        if user:
-            flash('Email already exists.', category='error')
-        elif len(email) < 4:
-            flash('Email must be greater than 3 characters.', category='error')
-        elif len(first_name) < 2:
-            flash('First name must be greater than 1 character.', category='error')
-        elif password1 != password2:
-            flash('Passwords don\'t match.', category='error')
-        elif len(password1) < 7:
-            flash('Password must be at least 7 characters.', category='error')
-        else:
-            new_user = User(email=email, first_name=first_name, password=generate_password_hash(
-                password1, method='sha256'))
-            db.session.add(new_user)
-            db.session.commit()
-            login_user(new_user, remember=True)
-            flash('Account created!', category='success')
-            return redirect(url_for('views.home'))
+             user = User.query.filter_by(email=email).first()
+             if user:
+                   flash('Email already exists.', category='error')
+             elif len(email) < 4:
+                    flash('email must be grater than 4 characters.', category='error')
+             elif len(first_name) < 2:
+                     flash('first name must be grater than 2 characters.', category='error')
+             elif password1 != password2:
+                     flash('passwords dont match.', category='error')
+             elif len(password1) < 7:
+                     flash('password must be grater than 7 characters.', category='error')
+             else:
+                     new_user = User(email=email, first_name=first_name, password=password1)
+                     db.session.add(new_user)
+                     db.session.commit()
+                     login_user(user, remember=True)
+                     flash('Account created.', category='success')
+                     return redirect(url_for('views.home'))
 
     return render_template("sign_up.html", user=current_user)
